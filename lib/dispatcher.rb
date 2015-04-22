fdad194f57d620b78504d9da6ba1a60d6960384c
Andrew Tomaka <atomaka@gmail.com>
2015-04-22 16:55:16 -0400

    Pass Rubocop complexity
    Hack assign_responders until it passes complexity.  Minimally, some of this
    needs to go into the Responder model.  Really, the dispatcher needs to be
    completely refactored.

--- lib/dispatcher.rb
+++ lib/dispatcher.rb
@@ -15,21 +15,39 @@ class Dispatcher
 
   def assign_responders(type)
     required_capacity = capacity(type)
-    available = Responder.available_on_duty(type).by_capacity
+    available = sorted_available_on_duty(type)
     current_capacity = available.capacity_sum
 
-    return if required_capacity == 0 || current_capacity == 0
+    return if assign_responders?(required_capacity, current_capacity)
 
     available.each do |responder|
-      break if required_capacity == 0
-      next unless responder.capacity <= required_capacity
+      break unless responders_needed(required_capacity)
+      next if responder_too_large(responder, required_capacity)
 
       add_responder(responder)
       available.delete(responder)
       required_capacity -= responder.capacity
     end
 
-    @emergency.responders << available.last if required_capacity > 0
+    add_responder(available.last) if responders_needed(required_capacity)
+  end
+
+  private
+
+  def sorted_available_on_duty(type)
+    Responder.available_on_duty(type).by_capacity
+  end
+
+  def responder_too_large(responder, required)
+    responder.capacity > required
+  end
+
+  def responders_needed(required)
+    required > 0
+  end
+
+  def assign_responders?(required, current)
+    required == 0 || current == 0
   end
 
   def set_full_response

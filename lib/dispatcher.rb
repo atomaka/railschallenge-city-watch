32db73482a9918d50a7b035d4ae57d31bffc17fb
Andrew Tomaka <atomaka@gmail.com>
2015-04-23 10:10:55 -0400

    Cleanup dispatcher

--- lib/dispatcher.rb
+++ lib/dispatcher.rb
@@ -8,58 +8,24 @@ class Dispatcher
       assign_responders(type)
     end
 
-    set_full_response if @emergency.capacity_met?
+    @emergency.complete_response if @emergency.capacity_met?
   end
 
   private
 
   def assign_responders(type)
-    required_capacity = capacity(type)
-    available = sorted_available_on_duty(type)
-    current_capacity = available.capacity_sum
+    available = Responder.sorted_available_on_duty(type)
 
-    return if assign_responders?(required_capacity, current_capacity)
+    return unless @emergency.responders_needed?(type)
+    return if available.size == 0
 
     available.each do |responder|
-      break unless responders_needed(required_capacity)
-      next if responder_too_large(responder, required_capacity)
+      break unless @emergency.responders_needed?(type)
+      next if @emergency.responder_too_large?(type, responder)
 
-      add_responder(responder)
-      available.delete(responder)
-      required_capacity -= responder.capacity
+      @emergency.add_responder(responder)
     end
 
-    add_responder(available.last) if responders_needed(required_capacity)
-  end
-
-  private
-
-  def sorted_available_on_duty(type)
-    Responder.available_on_duty(type).by_capacity
-  end
-
-  def responder_too_large(responder, required)
-    responder.capacity > required
-  end
-
-  def responders_needed(required)
-    required > 0
-  end
-
-  def assign_responders?(required, current)
-    required == 0 || current == 0
-  end
-
-  def set_full_response
-    @emergency.full_response = true
-    @emergency.save
-  end
-
-  def add_responder(responder)
-    @emergency.responders << responder
-  end
-
-  def capacity(type)
-    @emergency.send(Responder.types[type])
+    @emergency.add_responder(available.last) if @emergency.responders_needed?(type)
   end
 end
